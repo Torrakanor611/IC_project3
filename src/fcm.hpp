3d0ea02670ec8de8f0b33327aa87208063907a79
Pedro Abreu <pedro.abreu@ua.pt>
2022-01-10 16:21:34 +0000

    template fcm not working

---
+++ src/fcm.hpp
@@ -0,0 +1,21 @@
+#ifndef FCM_H
+#define FCM_H
+
+#include <map>
+
+using namespace std;
+
+/**
+ * 
+ */
+class FCM {
+    private:
+        int k;
+        double alfa;
+
+    public:
+        double entropy;
+
+};
+
+#endif
